9415f4800a15f3b16a3e4de47e4cb49160836e47
Nick Constante <nickmaverick07072007@gmail.com>
2026-03-17 14:19:20 +0100

    Java

--- Dam1_Nick/Programacion/POO_conFicheros/src/def/Main.java
+++ Dam1_Nick/Programacion/POO_conFicheros/src/def/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 String nombreFichero="tareas.txt";
-
+Tarea.leerFicheroTareas(nombreFichero);
+	//Tarea.mostrarTareas();
+	Tarea.grabarFicheroTareas(nombreFichero);
+	Tarea.ordenarTareas();
+	Tarea.mostrarTareas();
 	}
 
 }
--- Dam1_Nick/Programacion/POO_conFicheros/src/def/Tarea.java
+++ Dam1_Nick/Programacion/POO_conFicheros/src/def/Tarea.java
@@ -1,5 +1,8 @@
 package def;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 public class Tarea {
@@ -16,7 +19,65 @@ public class Tarea {
 		this.completada=completada;
 		Tarea.listaTareas.add(this);
 	}
+	
 	public static void leerFicheroTareas(String fichero) {
-		
+		try (BufferedReader lector=new BufferedReader(new FileReader(fichero))){
+			String linea;
+			while((linea=lector.readLine())!=null) {
+				String [] lista =linea.split(":");
+		boolean completado=true;
+		if(lista[3].equals("0"))
+			completado=false;
+		new Tarea(lista[0],lista[1],Integer.parseInt(lista[2]),completado);
+			}
+			
+		}catch(Exception e) {
+			System.out.println("Error: "+e.getMessage());
+		}
+	}
+	public static void grabarFicheroTareas(String fichero) {
+		try (PrintWriter pluma =new PrintWriter(fichero)){
+			for(Tarea tarea:listaTareas){
+					int completada=0;
+			if(tarea.completada==true)
+					completada=1;
+			pluma.printf("%s:%s:%d:%d",tarea.id,tarea.titulo,tarea.prioridad,completada);
+			pluma.println();
+			}
+		}catch(Exception e){
+			System.out.println("Error: "+e.getMessage());
+		}
+	}
+	
+	public  void mostrarTarea() {
+		String completada=" ";
+if(this.completada==true)
+	completada="X";
+		System.out.printf("%s [%s] %s (Prioridad: %d)\n",completada,this.id,this.titulo,this.prioridad);
+	}
+	public  static void mostrarTareas() {
+	for(Tarea tarea:Tarea.listaTareas)
+		tarea.mostrarTarea();
+	}
+	public static void ordenarTareas() {
+	ArrayList<Tarea>listaTareasCopia =new ArrayList<>();
+	do {
+		int mayor=-1;
+	 for(Tarea tarea:listaTareas) {
+		 if(tarea.prioridad>mayor)
+			 mayor=tarea.prioridad;
+			 
+	 }
+	 int posicion=0;
+	 for(int i=0;i<listaTareas.size();i++) {
+		 if(listaTareas.get(i).prioridad==mayor)
+			 posicion=i;
+	}
+	 listaTareasCopia.add(listaTareas.get(posicion));
+	 listaTareas.remove(posicion);
+	}while(listaTareas.size()!=0);
+			for(Tarea tareas:listaTareasCopia)
+				tareas.mostrarTarea();
+				
 	}
 }
