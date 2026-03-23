e43cfca26da8aa3b32e051dde070a8e47280fb53
Nick Constante <nickmaverick07072007@gmail.com>
2026-03-23 14:22:03 +0100

    Java

---
+++ Dam1_Nick/Programacion/Fichero_Aleatorio/src/def/Main.java
@@ -0,0 +1,124 @@
+package def;
+
+import java.io.RandomAccessFile;
+import java.util.HashMap;
+
+public class Main {
+static final int TAMANYO_NOMBRE=20;
+static final int TAMANYO_REGISTRO=(TAMANYO_NOMBRE *2 )+4;
+	public static void main(String[] args) {
+		String fichero ="agenda.dat";
+		HashMap<String,Integer> agenda=new HashMap<>();
+		agenda.put("Alejandro", 33);
+		agenda.put("Luis", 24);
+		agenda.put("Ana", 32);
+		agenda.put("Elvira", 41);
+		try {
+		crearAgenda(fichero,agenda);
+		leerRegistro(fichero,2);
+		leerRegistro(fichero,5);
+		modificarRegistro(fichero,2,"Ana Maria",33);
+		leerRegistro(fichero,2);
+		leerRegistro(fichero, 3);
+		// nuevoRegistro(fichero,"José Antonio",56);
+		leerRegistro(fichero,5);
+		System.out.println();
+		leerTodosLosRegistros(fichero);
+		}catch(Exception e) {
+			System.out.println("Error: "+e.getMessage());
+		}
+	}
+	
+	public static void leerTodosLosRegistros(String fichero) throws Exception {
+		try(RandomAccessFile raf = new RandomAccessFile(fichero,"r")){
+			int numRegistros=(int)raf.length()/TAMANYO_REGISTRO;	
+			
+		for(int i =1;i<numRegistros;i++) {
+			String nombre =leerNombre(raf);
+		int edad = raf.readInt();
+		System.out.printf("Registro: %d - Nombre: %s. Edad: %d\n",i,nombre,edad);
+		}
+	}
+	}
+	
+	public static void nuevoRegistro(String fichero ,String nombre,int edad)throws Exception {
+		try(RandomAccessFile raf=new RandomAccessFile(fichero,"rw")){
+			raf.seek(raf.length());
+			escribirNombre(raf, nombre);
+			raf.writeInt(edad);
+			System.out.println("Registro añadido correctamente");
+			
+		}
+	}
+	
+	public static void crearAgenda(String fichero,HashMap<String,Integer> agenda) throws Exception{
+		
+		try(RandomAccessFile raf= new RandomAccessFile(fichero,"rw")){
+		for(String nombre: agenda.keySet()) {
+			int edad=agenda.get(nombre);
+			escribirNombre(raf,nombre);
+			raf.writeInt(edad);
+		}
+		System.out.println("Agenda creada, Tamaño: " + raf.length() + " Bytes");
+		}
+		
+	}
+	
+	public static void escribirNombre(RandomAccessFile raf, String nombre) throws Exception {
+		char[] chars = new char[TAMANYO_NOMBRE];
+		for(int i=0;i<TAMANYO_NOMBRE;i++) {
+			if(i<nombre.length())
+				chars[i]=nombre.charAt(i);
+			else
+				chars[i]=' ';
+	}
+	
+	for(char c:chars)
+		raf.writeChar(c);
+}
+	
+	public static void leerRegistro(String fichero,int registro)throws Exception{
+		try(RandomAccessFile raf = new RandomAccessFile(fichero,"r")){
+			long posicion=TAMANYO_REGISTRO * (registro-1);
+			
+			if(posicion>=raf.length()) {
+				System.out.println("El Registro "+ registro +" no existe");
+				System.out.println("El registro mas alto es el "+ raf.length()/TAMANYO_REGISTRO);
+				
+			}
+			else {
+				raf.seek(posicion);
+			}
+			String nombre =leerNombre(raf);
+			int edad = raf.readInt();
+			System.out.printf("Registro: %d - Nombre: %s. Edad: %d\n",registro,nombre,edad);
+			}
+			}
+			
+	public static String leerNombre(RandomAccessFile raf) throws Exception{
+		String nombre="";
+	for(int i=0;i<TAMANYO_NOMBRE;i++) {	
+	char c = raf.readChar();
+	nombre=nombre+c;
+	}
+		return nombre.trim();
+	}
+	public static void modificarRegistro(String fichero, int registro,String nombre, int edad) throws Exception {
+		try(RandomAccessFile raf = new RandomAccessFile(fichero,"rw")){
+			long posicion=TAMANYO_REGISTRO * (registro-1);
+			
+			if(posicion>=raf.length()) {
+				System.out.println("El Registro "+ registro +" no existe");
+				System.out.println("El registro mas alto es el "+ raf.length()/TAMANYO_REGISTRO);
+				
+			}
+			else {
+				raf.seek(posicion);
+				escribirNombre(raf, nombre);
+				raf.writeInt(edad);
+				System.out.println("Registro " + registro + " modificado correctamente");
+			}
+	}
+	}
+}
+
---
+++ Dam1_Nick/Programacion/Fichero_Aleatorio/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Fichero_Aleatorio {
+}
